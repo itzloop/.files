5e98bce5512ead7f4295c57cb8763c575c2bbf98
itzloop <sina.sh.k78@gmail.com>
2025-02-02 17:52:53 +0330

    Changes: 1. Adds markdown-preview 2. Adds <leader>1 mapping for harpoon 3. Updates some packages

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -1,28 +1,38 @@
-return  {
+local function get_arguments()
+  return coroutine.create(function(dap_run_co)
+    local args = {}
+    vim.ui.input({ prompt = "Args: " }, function(input)
+      args = vim.split(input or "", " ")
+      coroutine.resume(dap_run_co, args)
+    end)
+  end)
+end
+
+return {
     {
         "folke/neodev.nvim",
         opts = {
-          library = {
-            enabled = true, -- when not enabled, neodev will not change any settings to the LSP server
-            -- these settings will be used for your Neovim config directory
-            runtime = true, -- runtime path
-            types = true, -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
-            -- plugins = true, -- installed opt or start plugins in packpath
-            -- you can also specify the list of plugins to make available as a workspace library
-            plugins = { "nvim-treesitter", "plenary.nvim", "telescope.nvim", "nvim-dap", "nvim-dap-ui" },
-          },
-          setup_jsonls = true, -- configures jsonls to provide completion for project specific .luarc.json files
-          -- for your Neovim config directory, the config.library settings will be used as is
-          -- for plugin directories (root_dirs having a /lua directory), config.library.plugins will be disabled
-          -- for any other directory, config.library.enabled will be set to false
-          -- override = function(root_dir, options) end,
-          -- With lspconfig, Neodev will automatically setup your lua-language-server
-          -- If you disable this, then you have to set {before_init=require("neodev.lsp").before_init}
-          -- in your lsp start options
-          lspconfig = true,
-          -- much faster, but needs a recent built of lua-language-server
-          -- needs lua-language-server >= 3.6.0
-          pathStrict = true,
+            library = {
+                enabled = true, -- when not enabled, neodev will not change any settings to the LSP server
+                -- these settings will be used for your Neovim config directory
+                runtime = true, -- runtime path
+                types = true, -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
+                -- plugins = true, -- installed opt or start plugins in packpath
+                -- you can also specify the list of plugins to make available as a workspace library
+                plugins = { "nvim-treesitter", "plenary.nvim", "telescope.nvim", "nvim-dap", "nvim-dap-ui" },
+            },
+            setup_jsonls = true, -- configures jsonls to provide completion for project specific .luarc.json files
+            -- for your Neovim config directory, the config.library settings will be used as is
+            -- for plugin directories (root_dirs having a /lua directory), config.library.plugins will be disabled
+            -- for any other directory, config.library.enabled will be set to false
+            -- override = function(root_dir, options) end,
+            -- With lspconfig, Neodev will automatically setup your lua-language-server
+            -- If you disable this, then you have to set {before_init=require("neodev.lsp").before_init}
+            -- in your lsp start options
+            lspconfig = true,
+            -- much faster, but needs a recent built of lua-language-server
+            -- needs lua-language-server >= 3.6.0
+            pathStrict = true,
         }
 
     },
@@ -34,24 +44,29 @@ return  {
             vim.keymap.set('n', '<F8>', function() require("dap").step_over() end, { noremap = true, silent = true })
             vim.keymap.set('n', '<F9>', function() require("dap").step_into() end, { noremap = true, silent = true })
             vim.keymap.set('n', '<F10>', function() require("dap").step_out() end, { noremap = true, silent = true })
-            vim.keymap.set('n', '<Leader>b', function() require("dap").toggle_breakpoint() end, { noremap = true, silent = true })
-            vim.keymap.set('n', '<Leader>B', function() require("dap").set_breakpoint() end, { noremap = true, silent = true })
-            vim.keymap.set('n', '<Leader>lp', function() require("dap").set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end, { noremap = true, silent = true })
-            vim.keymap.set('n', '<Leader>dr', function() require("dap").repl.open() end, { noremap = true, silent = true })
+            vim.keymap.set('n', '<Leader>b', function() require("dap").toggle_breakpoint() end,
+                { noremap = true, silent = true })
+            vim.keymap.set('n', '<Leader>B', function() require("dap").set_breakpoint() end,
+                { noremap = true, silent = true })
+            vim.keymap.set('n', '<Leader>lp',
+                function() require("dap").set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end,
+                { noremap = true, silent = true })
+            vim.keymap.set('n', '<Leader>dr', function() require("dap").repl.open() end,
+                { noremap = true, silent = true })
             vim.keymap.set('n', '<Leader>dl', function() require("dap").run_last() end, { noremap = true, silent = true })
-            vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
-              require('require("dap").ui.widgets').hover()
+            vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
+                require('require("dap").ui.widgets').hover()
             end, { noremap = true, silent = true })
-            vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
-              require('require("dap").ui.widgets').preview()
+            vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
+                require('require("dap").ui.widgets').preview()
             end, { noremap = true, silent = true })
             vim.keymap.set('n', '<Leader>df', function()
-              local widgets = require('require("dap").ui.widgets')
-              widgets.centered_float(widgets.frames)
+                local widgets = require('require("dap").ui.widgets')
+                widgets.centered_float(widgets.frames)
             end, { noremap = true, silent = true })
             vim.keymap.set('n', '<Leader>ds', function()
-              local widgets = require('require("dap").ui.widgets')
-              widgets.centered_float(widgets.scopes)
+                local widgets = require('require("dap").ui.widgets')
+                widgets.centered_float(widgets.scopes)
             end, { noremap = true, silent = true })
         end,
     },
@@ -59,5 +74,19 @@ return  {
         "theHamsta/nvim-dap-virtual-text",
         dependencies = { "mfussenegger/nvim-dap", "nvim-treesitter" }
     },
+    {
+        "leoluz/nvim-dap-go",
+        opts = {
+            dap_configurations = {
+                {
+                    args = get_arguments,
+                    mode = "debug",
+                    name = "Debug Package (Arguments)",
+                    program = "${fileDirname}",
+                    request = "launch",
+                    type = "go"
+                }
+            }
+        }
+    }
 }
-
--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -8,6 +8,7 @@ return {
         vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
         vim.keymap.set("n", "<Tab>", ui.nav_next)
         vim.keymap.set("n", "<S-Tab>", ui.nav_prev)
+        vim.keymap.set("n", "<leader>1", function () ui.nav_file(1) end)
         vim.keymap.set("n", "<leader>2", function () ui.nav_file(2) end)
         vim.keymap.set("n", "<leader>3", function () ui.nav_file(3) end)
         vim.keymap.set("n", "<leader>4", function () ui.nav_file(4) end)
